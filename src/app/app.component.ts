c6eca92563465aee8d5be67133e56abda9d8f075
edsson.diaz <edsson.diaz@getechnologiesmx.com>
2022-04-07 18:33:28 -0500

    -cambio para soportar la ',' en chrome mobil

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,56 @@
-import { Component } from '@angular/core';
+import {COMMA, ENTER} from '@angular/cdk/keycodes';
+import {Component, ViewChild} from '@angular/core';
+import {MatChipInputEvent} from '@angular/material/chips';
 
+export interface Fruit {
+  name: string;
+}
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'chip';
+  addOnBlur = false;
+  readonly separatorKeysCodes = [ENTER, COMMA, 299] as const;
+  fruits: Fruit[] = [{name: 'Lemon'}, {name: 'Lime'}, {name: 'Apple'}];
+  fuitToAdd:string = '';
+  readonly COMMA_CHAR = ',';
+
+  @ViewChild('cheapInput')
+  cheapInput!:HTMLInputElement;
+
+
+  add(event: MatChipInputEvent): void {
+     console.log(event);
+    const value = (event.value || '').trim();
+
+    if (value) {
+      this.fruits.push({name: value});
+    }
+
+    if(event.chipInput){
+      event.chipInput!.clear();
+    }
+
+  }
+
+
+  //for mobile chrome issue
+  checkCharacter(){
+    let fruit = this.fuitToAdd.trim();
+    let character = fruit.substring(fruit.length-1);
+    if(character == this.COMMA_CHAR){
+      this.add({input: this.cheapInput, value:fruit.substring(0,fruit.length-1)});
+      this.fuitToAdd = '';
+    }
+  }
+
+  remove(fruit: Fruit): void {
+    const index = this.fruits.indexOf(fruit);
+
+    if (index >= 0) {
+      this.fruits.splice(index, 1);
+    }
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,33 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatChipsModule} from '@angular/material/chips';
+import {MatAutocompleteModule} from '@angular/material/autocomplete';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {MatIconModule} from '@angular/material/icon';
+import {MatFormFieldModule} from '@angular/material/form-field';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent
+    ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        BrowserAnimationsModule,
+        MatChipsModule,
+        MatAutocompleteModule,
+        ReactiveFormsModule,
+        MatIconModule,
+        MatFormFieldModule,
+        FormsModule
+
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
